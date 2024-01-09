b48741dfc987b34a214b8c6855eca20197e3b25e
KeremDUZENLI <krmdznl97@gmail.com>
2024-01-09 16:44:48 +0100

    feat: most common words on csv

--- main.py
+++ main.py
@@ -3,23 +3,52 @@ import numpy as np
 from PIL import Image
 from bs4 import BeautifulSoup
 from wordcloud import WordCloud
+from collections import Counter
+import nltk
+from nltk.corpus import stopwords
+import string
+import csv
 
+nltk.download('stopwords')
+nltk.download('punkt')
 
-mask_image_location = input("Enter the mask image location: ")
-mask = Image.open(f"{mask_image_location}")
-mask_arr = np.array(mask)
+website_urls = [
+    "https://www.constantcontact.com/",
+    "https://www.interstellarrecords.at/",
+    "https://kuprosauwald.org/"
+]
 
+csv_file_name = "results.csv"
 
-website_url = input("Enter the URL: ")
-html_content = requests.get(website_url).text
-website_text = BeautifulSoup(html_content, 'html.parser').get_text()
+for website_url in website_urls:
+    html_content = requests.get(website_url).text
+    website_text = BeautifulSoup(html_content, 'html.parser').get_text()
 
-delete_text = ["\"", "//", "'", "*", "\n", "\t",
-               "xml version='1.0' encoding='utf-8'?", "html", ""]
-for text in delete_text:
-    website_text = website_text.replace(text, "")
+    delete_text = ["\"", "//", "'", "*", "\n", "\t", "–", "“", "„", "€", "$",
+                   "xml version='1.0' encoding='utf-8'?", "html", ""] + list(string.punctuation)
+    for text in delete_text:
+        website_text = website_text.replace(text, "")
 
+    stop_words_german = set(stopwords.words('german'))
+    stop_words_english = set(stopwords.words('english'))
+    stop_words = stop_words_german.union(stop_words_english)
 
-mb = WordCloud(width=1000, height=1000, mask=mask_arr).generate(website_text)
-save_file = input("Enter output file name: ")
-mb.to_file(f"{save_file}.jpg")
+    filtered_text = ' '.join([word for word in nltk.word_tokenize(
+        website_text) if word.lower() not in stop_words and not any(char.isdigit() for char in word) and len(word) > 2])
+
+    word_freq = Counter(filtered_text.split())
+    top_20_words = word_freq.most_common(20)
+
+    with open(csv_file_name, mode='a', encoding='utf-8', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+
+        if csv_file.tell() == 0:
+            writer.writerow(['Most Common Words', 'Frequency'])
+            writer.writerow([])
+
+        writer.writerow(['Website', website_url])
+
+        for word, freq in top_20_words:
+            writer.writerow([word, freq])
+
+        writer.writerow([])
